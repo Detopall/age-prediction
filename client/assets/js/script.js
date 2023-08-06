1116fb96eb25b5715fafc3d68644124b3033361f
denis.topallaj <denis.topallaj@student.howest.be>
2023-08-06 22:13:48 +0200

    modified: used another model to predict the age (huggingface), this does it better. Rewrote the client to recieve an object of labels, css looks also better.

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -23,6 +23,7 @@ function handleImageChange(e) {
 
 function handleSubmit(e) {
 	e.preventDefault();
+	document.getElementById('result').innerHTML = `Loading...`;
 	const form = e.target;
 	const data = new FormData(form);
 
@@ -32,11 +33,38 @@ function handleSubmit(e) {
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
-			document.getElementById('result').innerHTML = `Predicted Age: ${parseInt(data.age)}`;
+			displayPredictionResults(data);
 		})
 		.catch((error) => {
 			console.error('Error:', error);
-			document.getElementById('result').innerHTML = `The image provided is not in a valid format or the server has a problem. Please try again.`;
+			document.getElementById('result').innerHTML = `There is a problem. Please try again.`;
 		});
+}
+
+function displayPredictionResults(data) {
+	const resultContainer = document.getElementById('result');
+	resultContainer.innerHTML = '';
+
+	for (const entry of data) {
+		const label = entry.Label;
+		const probability = entry.Probability;
+
+		let barColor = "";
+		if (probability >= 0.4) {
+			barColor = 'green';
+		} else if (probability > 0.2) {
+			barColor = 'orange';
+		} else {
+			barColor = 'red';
+		}
+
+		const percentage = `${(probability * 100).toFixed(2)}%`;
+
+		const html = `
+		<div class="bar-container ${barColor}">
+			<span>${label} (${percentage})</span>
+		</div>`	
+
+		resultContainer.insertAdjacentHTML("beforeend", html);
+	}
 }
--- server/app.py
+++ server/app.py
@@ -2,29 +2,43 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+from transformers import AutoFeatureExtractor, AutoModelForImageClassification
 
 app = Flask(__name__)
 CORS(app)
 
-model = load_model('age-model.h5')
+transforms = AutoFeatureExtractor.from_pretrained("nateraw/vit-age-classifier")
+hugging_model = AutoModelForImageClassification.from_pretrained("nateraw/vit-age-classifier")
 
 @app.route('/prediction', methods=['POST'])
 def predict():
     image_file = request.files['image']
-    image = Image.open(image_file)
-    preprocessed_image = preprocess_image(image)
-    age_prediction = model.predict(preprocessed_image)
-    return jsonify({'age': float(age_prediction[0])})
-
+    return preprocess_image(image_file)
 
 def preprocess_image(image):
-    resized_image = image.resize((120, 120))
-    image_array = np.array(resized_image)
-    normalized_image = image_array / 255.0
-    preprocessed_image = np.expand_dims(normalized_image, axis=0)
-    return preprocessed_image
+	img = Image.open(image)
+
+	inputs = transforms(img, return_tensors='pt')
+	output = hugging_model(**inputs)
+	proba = output.logits.softmax(1)
+	
+	return create_probabilities(proba)
+
+
+def create_probabilities(proba):
+    class_labels = ["0-2", "3-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "more than 70"]
+    result = []
+
+    proba_list = proba[0].tolist()
+    label_proba_pairs = zip(class_labels, proba_list)
+    sorted_label_proba_pairs = sorted(label_proba_pairs, key=lambda x: x[1], reverse=True)
+
+    for label, prob in sorted_label_proba_pairs:
+        formatted_prob = float("{:.2f}".format(prob))
+        result.append({"Label": label, "Probability": formatted_prob})
+
+    return result
+
 
 
 if __name__ == '__main__':
